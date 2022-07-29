e5a4ca9fa24e7dc885bc7e8ec219794843001485
altamir95 <altamir95@yandex.ru>
2022-07-29 12:24:01 +0300

    ку

--- GisArshinRequester/GisArshinRequesterConsole/Program.cs
+++ GisArshinRequester/GisArshinRequesterConsole/Program.cs
@@ -1,11 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Net.Http.Json;
-using GisArshinRequesterLib;
+﻿using GisArshinRequesterLib;
 
 var si = new InformationRegisterAboutSiVerificationResults();
-var res =await si.Get(year: 2022, skip: 1, take: 10, SortType.Asc);
-var obj = await res.Content.ReadFromJsonAsync<Root<SiVerificationInfoResult>>();
+var res = await si.GetByType(orgName: "25913-08", year: 2020, skip: 1, take: 10, SortType.Asc);
 
-Console.ReadLine();
-
+foreach (var item in res)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Идентификатор элемента: {item.Id}");
+    Console.WriteLine($"Наименование организации-поверителя: {item.OrgName}");
+    Console.WriteLine($"Регистрационный номер типа СИ: {item.RegistrationNumber}");
+    Console.WriteLine($"Наименование типа СИ: {item.TypeName}");
+    Console.WriteLine($"Обозначение типа СИ: {item.TypeNotation}");
+    Console.WriteLine($"Модификация СИ: {item.Modification}");
+    Console.WriteLine($"Заводской/серийный номер/буквенноцифровое обозначение: {item.Number}");
+    Console.WriteLine($"Дата поверки: {item.VerificationDate}");
+    Console.WriteLine($"Действительна до: {item.ValidDate}");
+    Console.WriteLine($"Номер свидетельства/извещения/выписки: {item.DocumentNumber}");
+    Console.WriteLine($"Пригодность: {item.Applicability}");
+    Console.WriteLine();
+}
--- GisArshinRequester/GisArshinRequesterLib/InformationRegisterAboutSiVerificationResults.cs
+++ GisArshinRequester/GisArshinRequesterLib/InformationRegisterAboutSiVerificationResults.cs
@@ -1,41 +1,44 @@
-﻿namespace GisArshinRequesterLib
+﻿// https://fgis.gost.ru/fundmetrology/eapi/vri?year=2020&start=5&rows=20
+using System.Net.Http.Json;
+
+namespace GisArshinRequesterLib
 {
     public class InformationRegisterAboutSiVerificationResults
     {
-        // https://fgis.gost.ru/fundmetrology/eapi/vri?year=2020&start=5&rows=20
         public string UrlScheme = "https://";
         public string UrlHost = "fgis.gost.ru";
         public List<string> UrlPath = new() { "fundmetrology", "eapi" };
 
         private static readonly HttpClient client = new HttpClient();
 
-        public async Task<HttpResponseMessage> Get(int year,int skip,int take,SortType sort)
+        public async Task<List<SiVerificationInfoResult>> GetByType(string orgName, int year = 0, int skip = 0, int take = 10, SortType sort = SortType.Asc)
         {
-
-            var r = new Dictionary<string, string>
-            {
-                { "year", $"{year}" },
-                { "start", $"{skip}" },
-                { "rows", $"{take}" }
-                //{ "sort", sort.ToString().ToLower() }
-            };
-
-            var quary = UrlPathBuilder(r);
-            var uri = GetUrl(quary, new() { "vri" });
-
-            var result = await client.GetAsync(uri);
-
-
-            return result;
+            var siList = await Get(year: 2022, skip: 1, take: 10, SortType.Asc, search: orgName);
+            return siList;
         }
 
-        private string GetUrl(string quary,List<string> paths=null)
+        public async Task<List<SiVerificationInfoResult>> Get(int year = 0, int skip = 0, int take = 10, SortType sort = SortType.Asc, string? search = null)
+        {
+            if (year == 0)
+                year = DateTime.Now.Year;
+
+            var quary = UrlPathBuilder(year, skip, take, sort, search);
+            var uri = GetUrl(quary, new() { "vri" });
+
+            var respMess = await client.GetAsync(uri);
+            var respMessFromJson = await respMess.Content.ReadFromJsonAsync<Root<SiVerificationInfoResult>>();
+
+            return respMessFromJson?.Result?.Items;
+        }
+
+        private string GetUrl(string quary, List<string> paths = null)
         {
             var finalPath = UrlPath;
             if (paths is not null || paths?.Count > 0)
                 finalPath.AddRange(paths);
 
-            var url = new UriBuilder() {
+            var url = new UriBuilder()
+            {
                 Scheme = UrlScheme,
                 Host = UrlHost,
                 Path = string.Join("/", finalPath),
@@ -45,8 +48,18 @@
             return url;
         }
 
-        private string UrlPathBuilder(Dictionary<string,string> keyValues)
+        private string UrlPathBuilder(int year, int skip, int take, SortType sort, string search)
         {
+            var keyValues = new Dictionary<string, string>();
+
+            if (!string.IsNullOrEmpty(search))
+                keyValues.Add("search", $"{search}");
+
+            keyValues.Add("year", $"{year}");
+            keyValues.Add("start", $"{skip}");
+            keyValues.Add("rows", $"{take}");
+
+
             var pathsList = keyValues.Select(kv => $"{ kv.Key}={kv.Value}");
             var pathStr = string.Join("&", pathsList);
             return pathStr;
